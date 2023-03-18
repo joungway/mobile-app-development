584bbc51875089afe72bee9e64e3a7269999b322
joungway <itsweizhang@outlook.com>
2023-03-18 12:50:02 -0700

    commit

--- app/src/main/java/edu/northeastern/myapplication/Locator.java
+++ app/src/main/java/edu/northeastern/myapplication/Locator.java
@@ -1,14 +1,183 @@
 package edu.northeastern.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+//import android.os.PersistableBundle;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.Locale;
 
 public class Locator extends AppCompatActivity {
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private static final String DISTANCE_TRAVELED_KEY = "distance_traveled_key";
+
+    private TextView currentLocationTextView;
+    private TextView distanceTraveledTextView;
+    private Button resetDistanceButton;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private LocationRequest locationRequest;
+    private Location lastLocation;
+    private double totalDistanceTraveled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locator);
+
+        if (savedInstanceState != null) {
+            totalDistanceTraveled = savedInstanceState.getDouble(DISTANCE_TRAVELED_KEY);
+        }
+
+        currentLocationTextView = findViewById(R.id.current_location_textview);
+        distanceTraveledTextView = findViewById(R.id.distance_traveled_textview);
+        resetDistanceButton = findViewById(R.id.reset_distance_button);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(500);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+
+                for (Location location : locationResult.getLocations()) {
+                    updateLocation(location);
+                }
+            }
+        };
+
+        resetDistanceButton.setOnClickListener(v -> {
+            totalDistanceTraveled = 0;
+            updateDistanceTraveledUI();
+        });
+
+        checkPermissionsAndStartLocationUpdates();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putDouble(DISTANCE_TRAVELED_KEY, totalDistanceTraveled);
+    }
+
+    private void checkPermissionsAndStartLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            } else {
+                showPermissionRequiredDialog();
+            }
+        }
+    }
+
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, getMainLooper());
+        }
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void updateLocation(Location location) {
+        if (lastLocation != null) {
+            float distance = location.distanceTo(lastLocation);
+            totalDistanceTraveled += distance;
+            updateDistanceTraveledUI();
+        }
+
+        lastLocation = location;
+        currentLocationTextView.setText(String.format(Locale.getDefault(), "Current Location: (%.5f, %.5f)", location.getLatitude(), location.getLongitude()));
+    }
+
+    private void updateDistanceTraveledUI() {
+        distanceTraveledTextView.setText(String.format(Locale.getDefault(), "Distance Traveled: %.2f meters", totalDistanceTraveled));
+    }
+
+    private void showPermissionRequiredDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Location Permission Required")
+                .setMessage("Location permission is required to display and track your location.")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        checkPermissionsAndStartLocationUpdates();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        showExitConfirmationDialog();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    private void showExitConfirmationDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Exit Confirmation")
+                .setMessage("Are you sure you want to exit? Total distance will be lost.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .create()
+                .show();
+    }
+
+    @Override
+    public void onBackPressed() {
+        showExitConfirmationDialog();
     }
 }
